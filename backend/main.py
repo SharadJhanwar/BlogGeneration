f0ef32267e8ee1159ba5bf5d9f9d8681ec02b222
SharadJhanwar <sharadjhawar12345@gmail.com>
2025-12-30 00:15:37 +0530

    add CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI(title="GenAI blog generation")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # import api routers
 from app.api.v1.blog import router as blog_router
